f33e39cc34410e06b6ee5f21722af4f77e4970a6
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2025-02-23 23:48:09 +0800

    feat: adding resource controller for books and displaying list of books

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index(Request $request)
+    {
+        $title = $request->input('title');
+
+        // when: running a callback when the title is truthy value
+        $books = Book::when(
+            $title,
+            fn($query, $title) => $query->title($title)
+        )->get();
+
+        return view('books.index', ['books' => $books]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Book $book)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// automatically register route for CRUD and connect to controller method
+Route::resource('books', BookController::class);
+
+/*
+ Route::get('books', [BookController::class, 'index'])->name('books.index');
+ Route::get('books/create', [BookController::class, 'create'])->name('books.create');
+ Route::post('books', [BookController::class, 'store'])->name('books.store');
+ Route::get('books/{product}/edit', [BookController::class, 'edit'])->name('books.edit');
+ Route::put('books/{product}', [BookController::class, 'update'])->name('books.update');
+ Route::delete('books/{product}', [BookController::class, 'destroy'])->name('books.destroy');
+ */
