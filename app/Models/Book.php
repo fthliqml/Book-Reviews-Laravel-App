ba2d28190ec65f174709de2b9e4c8c14f004a355
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2025-02-23 22:52:29 +0800

    feat: add more logic for local query scopes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,37 +2,63 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
-    use HasFactory;
+	use HasFactory;
 
-    // Define that book can have many reviews
-    public function reviews()
-    {
-        return $this->hasMany(Review::class);
-    }
+	// Define that book can have many reviews
+	public function reviews()
+	{
+		return $this->hasMany(Review::class);
+	}
 
-    /**`
-     * Local Query Scopes
-     */
-    public function scopeTitle(Builder $query, string $title)
-    {
-        return $query->where('title', 'LIKE', "%{$title}%");
-    }
+	/**`
+	 * Local Query Scopes
+	 */
+	public function scopeTitle(Builder $query, string $title)
+	{
+		return $query->where('title', 'LIKE', "%{$title}%");
+	}
 
-    public function scopePopular(Builder $query): Builder
-    {
-        return $query->withCount('reviews')
-            ->orderBy('reviews_count', 'desc');
-    }
+	public function scopePopular(Builder $query, $from = null, $to = null): Builder
+	{
+		return $query
+			->withCount([
+				'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+			])
+			->orderBy('reviews_count', 'desc');
+	}
 
-    public function scopeHighestRated(Builder $query): Builder
-    {
-        return $query->withAvg('reviews', 'rating')
-            ->orderBy('reviews_avg_rating', 'desc');
-    }
+	public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
+	{
+		return $query
+			->withCount([
+				'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+			])
+			->withAvg([
+				'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+			], 'rating')
+			->orderBy('reviews_avg_rating', 'desc');
+	}
+
+	public function scopeMinReviews(Builder $query, int $minReviews)
+	{
+		return $query->having('reviews_count', '>=', $minReviews);
+	}
+
+	// can be access in this class
+	private function dateRangeFilter(Builder $query, $from = null, $to = null)
+	{
+		if ($from && !$to) {
+			$query->where('created_at', '>=', $from);
+		} else if (!$from && $to) {
+			$query->where('created_at', '<=', $to);
+		} else if ($from && $to) {
+			$query->whereBetween('created_at', [$from, $to]);
+		}
+	}
 }
