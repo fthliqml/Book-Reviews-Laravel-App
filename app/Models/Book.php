2762f28fe7e2986928128a8e1c0dedecc396b88f
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2025-02-23 21:23:31 +0800

    feat: add local query scopes in Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,5 +14,25 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    /**`
+     * Local Query Scopes
+     */
+    public function scopeTitle(Builder $query, string $title)
+    {
+        return $query->where('title', 'LIKE', "%{$title}%");
+    }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
     }
 }
